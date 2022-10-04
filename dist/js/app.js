370d655574cce68cdf88dfea802dceccff00468c
Lucas Silva <lucas.silva_1994@outlook.com>
2022-10-04 06:26:31 -0300

    Criando a classe NegociacaoController

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,5 +1,3 @@
 import { Negociacao } from "./models/Negociacao.js";
 const negociacao = new Negociacao(new Date(), 100, 1000);
 console.log(negociacao.volume);
-console.log(negociacao.data);
-alert("Oi");
