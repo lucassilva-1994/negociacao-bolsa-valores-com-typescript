4cd1b67a6fe90e187eba6b767d260dd888b206ab
Lucas Silva <lucas.silva_1994@outlook.com>
2022-10-11 20:57:54 -0300

    Trabalhando com classe e métodos abstrados

--- app/views/MensagemView.ts
+++ app/views/MensagemView.ts
@@ -3,7 +3,7 @@ import { View } from './View.js';
 export class MensagemView extends View<string>{
     template(model: string): string{
         return `
-            <p class="alert alert-info">${model}</p>
+            <p class="alert alert-primary">${model}</p>
         `;
     }
 }
