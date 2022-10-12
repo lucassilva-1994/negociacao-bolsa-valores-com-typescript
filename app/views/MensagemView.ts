839e076c83771f9cc7dac4165aa62a60ef6317d2
Lucas Silva <lucas.silva_1994@outlook.com>
2022-10-12 09:30:16 -0300

    Trabalhando um pouco mais com os modificadores de acesso protected e private

--- app/views/MensagemView.ts
+++ app/views/MensagemView.ts
@@ -1,7 +1,7 @@
 import { View } from './View.js';
 
 export class MensagemView extends View<string>{
-    template(model: string): string{
+    protected template(model: string): string{
         return `
             <p class="alert alert-primary">${model}</p>
         `;
