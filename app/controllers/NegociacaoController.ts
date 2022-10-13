03490497c4cf174031a5fb75ce8f8e5c9bfff85a
Lucas Silva <lucas.silva_1994@outlook.com>
2022-10-13 07:03:15 -0300

    Implementando método estático no modelo Negociacao

--- app/controllers/NegociacaoController.ts
+++ app/controllers/NegociacaoController.ts
@@ -20,7 +20,11 @@ export class NegociacaoController{
     }
 
     public adiciona():void{
-        const negociacao = this.criarNegociacao();
+        const negociacao = Negociacao.criarNegociacao(
+            this.inputData.value,
+            this.inputQuantidade.value,
+            this.inputValor.value
+        );
         if(!this.diaUtil(negociacao.data)){
             this.mensagemView.update("Negociações só podem ser feitos em dias úteis.");
             return;
@@ -34,14 +38,6 @@ export class NegociacaoController{
     private diaUtil(data: Date){
         return data.getDay() > DiasDaSemana.DOMINGO 
         && data.getDay() < DiasDaSemana.SABADO;
-    }
-
-    private criarNegociacao(): Negociacao{
-        const exp = /-/g;
-        const date = new Date(this.inputData.value.replace(exp,','));
-        const quantidade = parseInt(this.inputQuantidade.value);
-        const valor = parseFloat(this.inputValor.value);
-        return new Negociacao(date,quantidade,valor);
     }
 
     private limparFormulario(): void{
--- dist/js/controllers/NegociacaoController.js
+++ dist/js/controllers/NegociacaoController.js
@@ -14,7 +14,7 @@ export class NegociacaoController {
         this.negociacoesView.update(this.negociacoes);
     }
     adiciona() {
-        const negociacao = this.criarNegociacao();
+        const negociacao = Negociacao.criarNegociacao(this.inputData.value, this.inputQuantidade.value, this.inputValor.value);
         if (!this.diaUtil(negociacao.data)) {
             this.mensagemView.update("Negociações só podem ser feitos em dias úteis.");
             return;
@@ -27,13 +27,6 @@ export class NegociacaoController {
     diaUtil(data) {
         return data.getDay() > DiasDaSemana.DOMINGO
             && data.getDay() < DiasDaSemana.SABADO;
-    }
-    criarNegociacao() {
-        const exp = /-/g;
-        const date = new Date(this.inputData.value.replace(exp, ','));
-        const quantidade = parseInt(this.inputQuantidade.value);
-        const valor = parseFloat(this.inputValor.value);
-        return new Negociacao(date, quantidade, valor);
     }
     limparFormulario() {
         this.inputData.value = '';
